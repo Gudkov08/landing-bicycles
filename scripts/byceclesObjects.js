7a33382aac09bd8663f553d8801635859efb9c6b
Andrey Gudkov <gudkoff555@yandex.ru>
2021-08-20 22:56:00 +0300

    ready for first check

---
+++ scripts/byceclesObjects.js
@@ -0,0 +1,56 @@
+const bicyclesObjects = [
+  {
+    name: 'Cervelo Caledonia-5',
+    image: './images/caledonia.png',
+    href: 'https://www.sigmasports.com/item/Cervelo/Caledonia-5-Ultegra-Disc-Road-Bike-2021/RDEN',
+    type: 'highway',
+  },
+  {
+    name: 'Cannondale Systemsix Himod',
+    image: './images/himod.png',
+    href: 'https://www.sigmasports.com/item/Cannondale/SystemSix-HiMOD-Ultegra-Di2-Disc-Road-Bike-2021/R82J',
+    type: 'highway',
+  },
+  {
+    name: 'Trek Domane SL-7',
+    image: './images/domane.png',
+    href: 'https://www.sigmasports.com/item/Trek/Domane-SL-7-Force-eTap-AXS-Disc-Road-Bike-2021/RULF',
+    type: 'highway',
+  },
+  {
+    name: 'Cervelo Aspero GRX 810',
+    image: './images/grx810.png',
+    href: 'https://www.sigmasports.com/item/Cervelo/Aspero-GRX-810-1x-Disc-Gravel-Bike-2021/RJDE',
+    type: 'gravel',
+  },
+  {
+    name: 'Specialized S-Works Diverge',
+    image: './images/diverge.png',
+    href: 'https://www.sigmasports.com/item/Specialized/S-Works-Diverge-Gravel-Bike-2020/NVJ9',
+    type: 'gravel',
+  },
+  {
+    name: 'Cannondale Topstone Lefty 3',
+    image: './images/cannondale.png',
+    href: 'https://www.sigmasports.com/item/Cannondale/Topstone-Carbon-Lefty-3-Disc-Gravel-Road-Bike-2021/PUC8',
+    type: 'gravel',
+  },
+  {
+    name: 'Specialized S-Works Shiv',
+    image: './images/shiv.png',
+    href: 'https://www.sigmasports.com/item/Specialized/S-Works-Shiv-Disc-Limited-Edition-Triathlon-Bike-2019/K8P9',
+    type: 'tt',
+  },
+  {
+    name: 'BMC Timemachine 01 ONE',
+    image: './images/timemachine.png',
+    href: 'https://www.sigmasports.com/item/BMC/Timemachine-01-One-Force-Disc-TT-Triathlon-Bike-2021/S835',
+    type: 'tt',
+  },
+  {
+    name: 'Cervelo P-Series',
+    image: './images/p-series.png',
+    href: 'https://www.sigmasports.com/item/Cervelo/P-Series-Ultegra-Di2-TT-Triathlon-Bike-2021/RM6Q',
+    type: 'tt',
+  },
+];
---
+++ scripts/script.js
@@ -0,0 +1,181 @@
+// change theme colors
+const sun = document.querySelector("#sun");
+const moon = document.querySelector("#moon");
+const linkTheme = document.querySelector("#theme-link");
+const indicatorTheme = document.querySelector(".theme-changer__indicator");
+
+const lightTheme = "./vars/theme-light.css";
+const darkTheme = "./vars/theme-dark.css";
+
+sun.addEventListener("click", function () { turnOnLight(); });
+
+function turnOnLight() {
+  let currTheme = linkTheme.getAttribute("href");
+
+  if(currTheme == lightTheme) {
+    return;
+  } else {
+    currTheme = lightTheme;
+  }
+
+  linkTheme.setAttribute("href", currTheme);
+  indicatorTheme.classList.add("theme-changer__indicator_switched_sun");
+  indicatorTheme.classList.remove("theme-changer__indicator_switched_moon");
+}
+
+moon.addEventListener("click", function () { turnOnDark(); });
+
+function turnOnDark() {
+  let currTheme = linkTheme.getAttribute("href");
+
+  if(currTheme == darkTheme) {
+    return;
+  } else {
+    currTheme = darkTheme;
+  }
+
+  linkTheme.setAttribute("href", currTheme);
+  indicatorTheme.classList.remove("theme-changer__indicator_switched_sun");
+  indicatorTheme.classList.add("theme-changer__indicator_switched_moon");
+}
+
+// replace theme-changer to mobile-menu
+const mobileMenu = document.querySelector(".mobile-menu");
+const pageWidth = document.documentElement.scrollWidth;
+const themeChanger = document.querySelector("#theme-changer");
+
+if (pageWidth <= 480) {
+  themeChanger.parentNode.removeChild(themeChanger);
+  mobileMenu.appendChild(themeChanger);
+}
+
+
+// mobile-menu
+const buttonMobileMenuOpen = document.querySelector("#header-mobile-menu_open");
+const buttonMobileMenuClose = document.querySelector("#header-mobile-menu_close");
+const linksMobileMenu = document.querySelectorAll(".mobile-menu__link");
+
+linksMobileMenu.forEach((item)=> {
+  item.addEventListener("click", function () {
+    closeMobileMenu();
+  });
+});
+
+buttonMobileMenuOpen.addEventListener("click", function () {
+  openMobileMenu();
+});
+
+buttonMobileMenuClose.addEventListener("click", function () {
+  closeMobileMenu();
+});
+
+function openMobileMenu() {
+  buttonMobileMenuOpen.style.display = "none";
+  buttonMobileMenuClose.style.display = "block";
+  mobileMenu.classList.add('mobile-menu_open');
+};
+
+function closeMobileMenu() {
+  buttonMobileMenuOpen.style.display = "block";
+  buttonMobileMenuClose.style.display = "none";
+  mobileMenu.classList.remove('mobile-menu_open');
+};
+
+
+//add bicycles-types objects
+const bicycleTypesContent = document.querySelector('.bicycle-types__content');
+const bicycleTypesTemplate = document.querySelector('#template__bicycle-types');
+
+
+function createBicycleTypes(object) {
+  clone = bicycleTypesTemplate.content.cloneNode(true);
+  clone.querySelector(".bicycle-types__title").textContent = object.title;
+  clone.querySelector(".bicycle-types__text").textContent = object.text;
+  clone.querySelector(".bicycle-types__image_position_left").style.backgroundImage = object.imageLeft;
+  clone.querySelector(".bicycle-types__image_position_right").style.backgroundImage = object.imageRight;
+}
+
+bicycleTypesObjects.forEach( (i)=> {
+  createBicycleTypes(i);
+  bicycleTypesContent.append(clone);
+});
+
+const bicycleTypesItems = document.querySelectorAll('.bicycle-types__content-item');
+
+bicycleTypesItems[0].classList.add('bicycle-types__content-item_visible');
+
+//bicycles-types slider
+const buttonLeft = document.querySelector('#button-left');
+const buttonRight = document.querySelector('#button-right');
+
+buttonLeft.addEventListener("click", function () { moveLeft(); });
+buttonRight.addEventListener("click", function () { moveRight(); });
+
+let slideIndex = 1;
+
+function moveRight() {
+  showSlides(slideIndex += 1);
+};
+
+function moveLeft() {
+  showSlides(slideIndex -= 1);
+};
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+};
+
+function showSlides(n) {
+  if (n > bicycleTypesItems.length) {
+    slideIndex = 1
+  } else if (n < 1) {
+    slideIndex = bicycleTypesItems.length
+  };
+
+  for (let slide of bicycleTypesItems) {
+    slide.classList.remove("bicycle-types__content-item_visible");
+  };
+
+  bicycleTypesItems[slideIndex - 1].classList.add("bicycle-types__content-item_visible");
+}
+
+//add bicycles-cards
+const bicyclesCardsContainer = document.querySelector('.bicycles__cards');
+const bicyclesCardsTemplate = document.querySelector('#template__bicycles-card');
+
+const highwayBicycles = bicyclesObjects.filter(item => item.type === 'highway');
+const gravelBicycles = bicyclesObjects.filter(item => item.type === 'gravel');
+const ttBicycles = bicyclesObjects.filter(item => item.type === 'tt');
+
+function createBicycleCard(object) {
+  clone = bicyclesCardsTemplate.content.cloneNode(true);
+  clone.querySelector(".bicycles__img-link").href = object.href;
+  clone.querySelector(".bicycles__img-link").ariaLabel = "Велосипед " + object.name;
+  clone.querySelector(".bicycles__img").src = object.image;
+  clone.querySelector(".bicycles__img").alt = "Велосипед " + object.name + ".";
+  clone.querySelector(".bicycles__card-capture_position_inside").textContent = object.name;
+  clone.querySelector(".bicycles__card-capture_position_outside").textContent = object.name;
+}
+
+highwayBicycles.forEach( (i)=> {
+  createBicycleCard(i);
+  bicyclesCardsContainer.append(clone);
+});
+
+//change bicycles-cards
+const highwayLink = document.querySelector("#highway-link");
+const gravelLink = document.querySelector("#gravel-link");
+const ttLink = document.querySelector("#tt-link");
+const bicyclesCards = document.querySelectorAll(".bicycles__card-item");
+
+function showBicyclesCards(massive) {
+  bicyclesCardsContainer.innerHTML = "";
+  massive.forEach( (i)=> {
+    createBicycleCard(i);
+    bicyclesCardsContainer.append(clone);
+  });
+}
+
+highwayLink.addEventListener("click", function () { showBicyclesCards(highwayBicycles); });
+gravelLink.addEventListener("click", function () { showBicyclesCards(gravelBicycles); });
+ttLink.addEventListener("click", function () { showBicyclesCards(ttBicycles); });
